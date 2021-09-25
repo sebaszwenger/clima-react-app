3ead5303dc1d51f04b3861d023f6a51f2ac33b78
sebaszwenger <sebaszwenger@gmail.com>
2021-09-25 12:50:30 -0300

    agregado boton submit, state de busqueda paso a APP, agregado useEffect para consultar API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 
 function App() {
+  //state de busqueda
+  const [busqueda, setBusqueda] = useState({
+    ciudad: "",
+    pais: "",
+  });
+
+  //state validacion para consultar API
+  const [consultar, setConsultar] = useState(false);
+
+  //Extrae ciudad y pais
+  const { ciudad, pais } = busqueda;
+
+  useEffect(() => {
+    const consultarAPI = async () => {};
+    consultarAPI();
+  }, [consultar]);
+
   return (
     <>
       <Header titulo="Clima React App" />
@@ -10,7 +29,11 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Formulario />
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
             </div>
             <div className="col m6 s12">2</div>
           </div>
